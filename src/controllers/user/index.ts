df642632fef12f59bca86ef94b2ca8fd7d83d7ba
Artem Chekhovskoi <neomonreo@gmail.com>
2023-10-10 17:11:55 +0300

    deleted postUpdateProfile route

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,6 +1,5 @@
 import postGoogleSignIn from "@controllers/user/postGoogleSignin";
 import postLogOut from "@controllers/user/postLogOut";
-import postUpdateProfile from "@controllers/user/postUpdateProfile";
 import postUpdateGender from "@controllers/user/postUpdateGender";
 import postUpdateLocation from "@controllers/user/postUpdateLocation";
 import postUpdateAppsConnected from "@controllers/user/postUpdateAppsConnected";
@@ -8,7 +7,6 @@ import postUpdateAppsConnected from "@controllers/user/postUpdateAppsConnected";
 const user = {
   postGoogleSignIn,
   postLogOut,
-  postUpdateProfile,
   postUpdateLocation,
   postUpdateAppsConnected,
   postUpdateGender,
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -5,7 +5,6 @@ const router = Router();
 
 router.route("/google-signin").post(controllers.user.postGoogleSignIn);
 router.route("/logout").post(controllers.user.postLogOut);
-router.route("/update-profile").post(controllers.user.postUpdateProfile);
 router.route("/update-gender").post(controllers.user.postUpdateGender);
 router.route("/update-location").post(controllers.user.postUpdateLocation);
 router
