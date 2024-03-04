e814e6c023b8d17da498a87e9658ae53771fe526
Artem Chekhovskoi <neomonreo@gmail.com>
2024-03-04 18:49:27 +0200

    auth logs

--- src/controllers/user/postGoogleSignin.ts
+++ src/controllers/user/postGoogleSignin.ts
@@ -6,72 +6,73 @@ import { socialLogin } from "./helpers/socialLogin";
 
 export type SocialType = "google" | "facebook" | "apple";
 export interface IGoogleProfile {
-  id: string;
-  email: string;
-  name: string;
-  verified_email: boolean;
-  given_name: string;
-  family_name: string;
-  picture: string;
-  locale: string;
-  userAgent?: string;
+	id: string;
+	email: string;
+	name: string;
+	verified_email: boolean;
+	given_name: string;
+	family_name: string;
+	picture: string;
+	locale: string;
+	userAgent?: string;
 }
 
 export interface IClientData {
-  userAgent?: string;
+	userAgent?: string;
 }
 
 interface ApiResponse {
-  success: boolean;
-  data: {
-    token?: string;
-    registrationStep?: string;
-    usersID?: string;
-    provider?: SocialType;
-  };
-  error: string;
-  errorCode: string;
+	success: boolean;
+	data: {
+		token?: string;
+		registrationStep?: string;
+		usersID?: string;
+		provider?: SocialType;
+	};
+	error: string;
+	errorCode: string;
 }
 const postGoogleSignIn = async (req: Request, res: Response) => {
-  const PROVIDER = "google";
-  const responseJSON: ApiResponse = {
-    success: false,
-    data: {},
-    error: "",
-    errorCode: "",
-  };
-  try {
-    const { token } = req.body;
+	const PROVIDER = "google";
+	const responseJSON: ApiResponse = {
+		success: false,
+		data: {},
+		error: "",
+		errorCode: "",
+	};
+	try {
+		const { token } = req.body;
 
-    // token validation
-    const isValidGoogleToken = isValidToken(token);
-    if (!isValidGoogleToken) {
-      responseJSON.error = "Invalid token!";
-      responseJSON.errorCode = "INVALID_TOKEN";
-      return res.status(400).json(responseJSON);
-    }
+		// token validation
+		const isValidGoogleToken = isValidToken(token);
+		if (!isValidGoogleToken) {
+			logger.error(`Error at controllers/postGoogleSign: Invalid token 1!`);
+			responseJSON.error = "Invalid token!";
+			responseJSON.errorCode = "INVALID_TOKEN";
+			return res.status(400).json(responseJSON);
+		}
 
-    const profile = await getDataFromGoogle(token);
+		const profile = await getDataFromGoogle(token);
 
-    if (!profile || !profile.id) {
-      responseJSON.error = "Invalid token!";
-      responseJSON.errorCode = "INVALID_TOKEN";
-      return res.status(400).json(responseJSON);
-    }
+		if (!profile || !profile.id) {
+			logger.error(`Error at controllers/postGoogleSign: Invalid token 2!`);
+			responseJSON.error = "Invalid token!";
+			responseJSON.errorCode = "INVALID_TOKEN";
+			return res.status(400).json(responseJSON);
+		}
 
-    const userData = await socialLogin(req, res, profile, "google");
+		const userData = await socialLogin(req, res, profile, "google");
 
-    responseJSON.success = true;
-    responseJSON.data = userData;
-    responseJSON.data.provider = PROVIDER;
-    return res.status(200).json(responseJSON);
-  } catch (error) {
-    logger.error(`Error at controllers/postGoogleSign: ${error}`);
-    logger.error(error);
-    responseJSON.error = "Internal server error";
-    responseJSON.errorCode = "INTERNAL_SERVER_ERROR";
-    return res.status(500).json(responseJSON);
-  }
+		responseJSON.success = true;
+		responseJSON.data = userData;
+		responseJSON.data.provider = PROVIDER;
+		return res.status(200).json(responseJSON);
+	} catch (error) {
+		logger.error(`Error at controllers/postGoogleSign: ${error}`, error);
+		responseJSON.error = "Internal server error";
+		responseJSON.errorCode = "INTERNAL_SERVER_ERROR";
+		return res.status(500).json(responseJSON);
+	}
 };
 
 export default postGoogleSignIn;
