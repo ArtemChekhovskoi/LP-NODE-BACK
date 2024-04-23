0f42bcdd18d27a62b08bb71ab41f5e8c88fb1efb
Artem Chekhovskoi <neomonreo@gmail.com>
2024-04-23 18:11:49 +0300

    sleep duration recalculated - 2

--- src/controllers/measurements/helpers/sumDailySleep.ts
+++ src/controllers/measurements/helpers/sumDailySleep.ts
@@ -1,6 +1,7 @@
 import getStartOfDay from "@helpers/getStartOfTheDay";
 import { ISleepSample } from "@controllers/measurements/appleHealth/heplers/saveAppleHealthSleep";
 import dayjs from "dayjs";
+import { sleep } from "../../../../_mocks/alldata";
 
 interface SleepDuration {
 	duration: number;
@@ -19,7 +20,7 @@ const sumDailySleep = (measurements: ISleepSample[]): { [key: string]: SleepDura
 			const isIncludedToNextDay =
 				dayjs(measurement.startDate).utc().hour() > DAILY_CUTOFF_HOURS ||
 				dayjs(measurement.endDate).utc().isAfter(dayjs(measurement.startDate).utc(), "day");
-			const sleepDuration = dayjs(measurement.endDate).diff(measurement.startDate, "minute");
+			const sleepDuration = dayjs(measurement.endDate).diff(measurement.startDate, "second");
 			let date = getStartOfDay(measurement.startDate).toISOString();
 			if (isIncludedToNextDay) {
 				date = dayjs(date).add(1, "day").toISOString();
@@ -33,23 +34,37 @@ const sumDailySleep = (measurements: ISleepSample[]): { [key: string]: SleepDura
 			} else {
 				if (
 					dayjs(acc[date].endDate).isAfter(dayjs(measurement.startDate)) &&
-					dayjs(measurement.endDate).isAfter(dayjs(acc[date].endDate))
+					(dayjs(measurement.endDate).isAfter(dayjs(acc[date].endDate)) ||
+						dayjs(measurement.endDate).isSame(dayjs(acc[date].endDate)))
 				) {
-					const periodDuration = dayjs(measurement.endDate).diff(acc[date].endDate, "minute");
+					const periodDuration = dayjs(measurement.endDate).diff(acc[date].endDate, "second");
 					acc[date].duration += periodDuration;
 					acc[date].endDate = measurement.endDate;
 					acc[date].startDate = measurement.startDate;
 					return acc;
 				}
-				acc[date].duration += sleepDuration;
-				acc[date].endDate = measurement.endDate;
-				acc[date].startDate = measurement.startDate;
+				if (!dayjs(measurement.endDate).isBefore(dayjs(acc[date].endDate))) {
+					acc[date].duration += sleepDuration;
+					acc[date].endDate = measurement.endDate;
+					acc[date].startDate = measurement.startDate;
+				}
 			}
 			return acc;
 		},
 		{} as { [date: string]: { startDate: Date | string; endDate: Date | string; duration: number } }
 	);
-	return resultsObj;
+	const returnObjInMinutes = Object.entries(resultsObj).reduce(
+		(acc, [date, measurement]) => {
+			acc[date] = {
+				duration: Math.round(measurement.duration / 60),
+			};
+			return acc;
+		},
+		{} as { [date: string]: SleepDuration }
+	);
+	return returnObjInMinutes;
 };
+
+sumDailySleep(sleep);
 
 export default sumDailySleep;
