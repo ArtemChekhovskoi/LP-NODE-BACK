ad4389ce383ff999cb27f4621c4fae9db913e905
Artem Chekhovskoi <neomonreo@gmail.com>
2025-01-14 22:30:33 +0200

    fix

--- src/middlewares/validateDTO.ts
+++ src/middlewares/validateDTO.ts
@@ -4,18 +4,18 @@ import { ApiError } from "@helpers/apiErrorsHandler";
 
 type DTOType = "body" | "query" | "params";
 const validateDTO = (schema: yup.ObjectSchema<any>, type: DTOType = "body") => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const validatedDTO = await schema.validate(req[type], {
-        abortEarly: false,
-        strict: true,
-      });
-      req[type] = validatedDTO;
-      next();
-    } catch (e: any) {
-      next(ApiError.incorrectParameter(e.message || "Incorrect parameter"));
-    }
-  };
+	return async (req: Request, res: Response, next: NextFunction) => {
+		try {
+			const validatedDTO = await schema.validate(req[type], {
+				abortEarly: false,
+				strict: true,
+			});
+			req[type] = validatedDTO;
+			next();
+		} catch (e: any) {
+			next(ApiError.incorrectParameter(e.message || "Incorrect parameter"));
+		}
+	};
 };
 
 export { validateDTO };
